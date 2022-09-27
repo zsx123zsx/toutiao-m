dea9262852545d98cf49b8450ae00958051948ee
zsx <619893077@qq.com>
2022-09-27 22:02:59 +0800

    项目初始化-调整初始化目录结构

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
